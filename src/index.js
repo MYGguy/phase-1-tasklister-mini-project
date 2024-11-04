67b2c85a9e4ebd3c12dc4a3d75a6fa4233422293
Moshe Goldstein <mygguy613@gmail.com>
2024-11-04 17:40:27 -0500

    create delete button

--- src/index.js
+++ src/index.js
@@ -10,12 +10,25 @@ function submitItem() {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    //grab and create new list item
     let li = document.createElement('li');
     let taskItem = document.getElementById('new-task-description').value;
-    li.textContent = taskItem;
+    li.textContent = `${taskItem} `;
 
+    //create delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.addEventListener('click', handleDelete
+    )
+    deleteBtn.textContent = 'X';
+    li.appendChild(deleteBtn);
+
+    //append item to page
     document.querySelector('#tasks').appendChild(li);
 
     form.reset();
   })
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
 }
