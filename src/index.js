c989a62094e9a3bc387ed95795a7960f77614853
Moshe Goldstein <mygguy613@gmail.com>
2024-11-04 17:30:16 -0500

    add items to list with submit btn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  submitItem();
 });
+
+
+function submitItem() {
+
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let li = document.createElement('li');
+    let taskItem = document.getElementById('new-task-description').value;
+    li.textContent = taskItem;
+
+    document.querySelector('#tasks').appendChild(li);
+  })
+}
